96dd92cd71ac1199f83a22ec9fb47d248feec4c4
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2019-02-11 22:23:55 -0200

    Componente de artigos

---
+++ src/components/Articles.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+const articles = [
+  {
+    title: 'Computador e Placas Antigas Nvidia em Pleno 2019',
+    author: 'Cézar Campos',
+    resume: 'Se você é um infeliz que possui um computador antigo e, o que é pior, uma placa de vídeo Nvidia, então bem vindo ao clube. Sim, está difícil achar uma distribuição Linux estável e que funcione bem nestas placas que usam o driver Legacy 304. A maioria das distros já deixaram o suporte e tiraram de seus repositórios. Pesquisei bem a fundo, porque isso acontece e como sobreviver em 2019, tendo um PC antigo com placas de vídeo da Nvidia. Mostrarei neste artigo o problema e como configurar os drivers em uma distro bonitinha.'
+  },
+  {
+    title: 'Computador e Placas Antigas Nvidia em Pleno 2019',
+    author: 'Cézar Campos',
+    resume: 'Se você é um infeliz que possui um computador antigo e, o que é pior, uma placa de vídeo Nvidia, então bem vindo ao clube. Sim, está difícil achar uma distribuição Linux estável e que funcione bem nestas placas que usam o driver Legacy 304. A maioria das distros já deixaram o suporte e tiraram de seus repositórios. Pesquisei bem a fundo, porque isso acontece e como sobreviver em 2019, tendo um PC antigo com placas de vídeo da Nvidia. Mostrarei neste artigo o problema e como configurar os drivers em uma distro bonitinha.'
+  },
+  {
+    title: 'Computador e Placas Antigas Nvidia em Pleno 2019',
+    author: 'Cézar Campos',
+    resume: 'Se você é um infeliz que possui um computador antigo e, o que é pior, uma placa de vídeo Nvidia, então bem vindo ao clube. Sim, está difícil achar uma distribuição Linux estável e que funcione bem nestas placas que usam o driver Legacy 304. A maioria das distros já deixaram o suporte e tiraram de seus repositórios. Pesquisei bem a fundo, porque isso acontece e como sobreviver em 2019, tendo um PC antigo com placas de vídeo da Nvidia. Mostrarei neste artigo o problema e como configurar os drivers em uma distro bonitinha.'
+  }
+]
+
+class Articles extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      articles: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({articles})
+  }
+
+  render() {
+    return (
+      <div>
+        Artigos
+        {this.state.articles.map(article =>
+          <div>
+            <span>{article.title}</span>
+            <span>{article.author}</span>
+            <span>{article.resume}</span>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default Articles;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Articles from '../components/Articles';
+import '../styles/Home.css';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <span>Home</span>
+        <ul className="nav nav-tabs">
+          <li className="active"><a data-toggle="tab" href="#artigos">Artigos</a></li>
+          <li className=""><a data-toggle="tab" href="#dicas">Dicas</a></li>
+          <li><a data-toggle="tab" href="#forum">Fórum</a></li>
+          <li className=""><a data-toggle="tab" href="#scripts">Scripts</a></li>
+        </ul>
+        <Articles/>
       </div>
     );
   }
